ddc754e0f819fed689f6f10df4735670cda41e57
PSCoder10462 <pscoder10462@gmail.com>
2021-05-10 18:40:33 +0530

    header nav bar design

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./assets/css/App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      {/* home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBBrUa-j-7gREBLYeR9__LTupiaq-B4csM",
+  authDomain: "clone-d34a6.firebaseapp.com",
+  projectId: "clone-d34a6",
+  storageBucket: "clone-d34a6.appspot.com",
+  messagingSenderId: "1009520188191",
+  appId: "1:1009520188191:web:55c2fc252106a8f875ee2a",
+  measurementId: "G-2SQY2RCSL3",
+};
